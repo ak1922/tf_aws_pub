165db049f1edd34227ef147c0bc43722af18fa4f
ak1922 <116603167+ak1922@users.noreply.github.com>
2024-06-27 18:34:47 -0400

    s3 bucket, encryption.

---
+++ storage/s3_kms/main.tf
@@ -0,0 +1,61 @@
+# Random number.
+resource "random_integer" "number" {
+  max = 4
+  min = 2
+}
+
+# s3 bucket
+resource "aws_s3_bucket" "buck" {
+  bucket              = local.bucket_name
+  object_lock_enabled = true
+
+  tags = merge({ "Name" = local.bucket_name }, local.module_tags)
+}
+
+# IAM role and https access.
+resource "aws_iam_role" "assume_role" {
+  name = "${aws_s3_bucket.buck.bucket}-AssumeRole"
+
+  assume_role_policy = jsonencode(
+    {
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          Action = "sts:AssumeRole"
+          Sid    = ""
+          Principal = {
+            Service = "s3.amazonaws.com"
+          }
+          Condition = {
+            Bool = {
+              "aws:SecureTransport" = true
+            }
+          }
+        },
+      ]
+    }
+  )
+}
+
+# s3 bucket encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encrypt" {
+  bucket = aws_s3_bucket.buck.bucket
+
+  rule {
+    bucket_key_enabled = true
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.encryption_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+# s3 bucket versioning.
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  bucket = aws_s3_bucket.buck.id
+}

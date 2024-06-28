899b7ed46ab649a3a6dbfe6b73e1e16483566345
ak1922 <116603167+ak1922@users.noreply.github.com>
2024-06-28 10:14:42 -0400

    IAM policy sns.

--- storage/s3_kms/data.tf
+++ storage/s3_kms/data.tf
@@ -1,1 +1,35 @@
+# Data source current user.
 data "aws_caller_identity" "current" {}
+
+# Data source IAM policy document for SNS topic
+data "aws_iam_policy_document" "sns_policy" {
+  policy_id = "s3notify_policy"
+
+  statement {
+    sid    = "s3notify_policy"
+    effect = "Allow"
+
+    resources = [
+      aws_sns_topic.sns_topic.arn
+    ]
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "s3.amazonaws.com"
+      ]
+    }
+
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+  }
+}

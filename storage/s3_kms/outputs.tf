9967fa12b0537742bc78602e05a152fc71caea8e
ak1922 <116603167+ak1922@users.noreply.github.com>
2024-06-28 10:15:12 -0400

    New block.

--- storage/s3_kms/outputs.tf
+++ storage/s3_kms/outputs.tf
@@ -2,6 +2,14 @@ output "encryption_key_id" {
   value = aws_kms_key.encryption_key.id
 }
 
-output "s3_encrypt" {
-    value = aws_s3_bucket_server_side_encryption_configuration.s3_encrypt.id
+output "s3_encrypt_id" {
+  value = aws_s3_bucket_server_side_encryption_configuration.s3_encrypt.id
+}
+
+output "snstopic_arn" {
+  value = aws_sns_topic.sns_topic.arn
+}
+
+output "topicpolicy_id" {
+  value = aws_sns_topic_policy.topic_policy.id
 }
